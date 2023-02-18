a6ae1ba840e6fae13a9b830ea16d59f15a46519e
Doht2003 <bangngoc535@gmail.com>
2023-02-18 11:11:36 +0700

    fix: file pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "/Luyen_Tay/Portfolio/src/Assets/CV_Hoang_Thi_Do_Frontend_Developer.pdf";
+import pdf from "../../Assets/CV_Hoang_Thi_Do_Frontend_Developer.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
